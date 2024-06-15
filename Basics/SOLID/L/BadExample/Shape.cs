f52d7655c5d52950ecdfea41433087fd4070fdf6
ShubhMore <Shubham.More@Klingelnberg.com>
2024-06-15 18:54:00 +0530

    Add bad example for Liskov Substitution principle.

---
+++ Basics/SOLID/L/BadExample/Shape.cs
@@ -0,0 +1,7 @@
+namespace Basics.SOLID.L.BadExample
+{
+    public abstract class Shape
+    {
+        public abstract double Area { get; }
+    }
+}
---
+++ Basics/SOLID/L/BadExample/Square.cs
@@ -0,0 +1,17 @@
+namespace Basics.SOLID.L.BadExample
+{
+    public class Square : Rectangle
+    {
+        public override double Width
+        {
+            get => base.Width;
+            set => base.Width = base.Height = value;
+        }
+
+        public override double Height
+        {
+            get => base.Height;
+            set => base.Height = base.Width = value;
+        }
+    }
+}
